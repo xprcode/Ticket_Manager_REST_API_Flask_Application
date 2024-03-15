b6ce66e2fc7bbbcf202eba5cc70c30db64ac427c
Piotr Kasperek <xprcode@onet.pl>
2024-03-15 12:36:44 +0100

    Adding message to email and testing the message in Mailtrap

--- rest_api_application/email_manager/main.py
+++ rest_api_application/email_manager/main.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-import email
-from os import getenv
 from string import Template
+from os import getenv
 from dotenv import load_dotenv
-
+import email
 from rest_api_application.email_manager.user_and_event import get_user_and_event
 from rest_api_application.email_manager.emails import Credentials, EmailSender
+
 
 load_dotenv()
 
@@ -21,22 +21,27 @@ credentials = Credentials(username, password)
 
 def send_remainder_to_borrowers(user_event, connection):
     
-        template = Template('''Test
+    template = Template('''Dear $name. 
+    Thank you for adding $event_name to your events.
+    The $event_name will take place at $event_adress on $event_date.
+    Once again thank you and see you at $event_adress .
+    Best regards,
+    Ticket_Manager_Team!
         ''')
-        text = template.substitute({
+    text = template.substitute({
             'name': user_event.name,
             'email': user_event.email,
             'event_name': user_event.event_name,
-            'event adress': user_event.event_adress,
-            'event_data': user_event.event_data
+            'event_adress': user_event.event_adress,
+            'event_date': user_event.event_data
         })
-
-        message = email.message_from_string(text)
-        message.set_charset('utf-8')
-        message['From'] = sender
-        message['To'] = user_event.email
-        message['Subject'] = 'Test'
-        connection.sendmail(sender, user_event.email, message.as_string())
+ 
+    message = email.message_from_string(text)
+    message.set_charset('utf-8')
+    message['From'] = sender
+    message['To'] = user_event.email
+    message['Subject'] = f'New event - {user_event.event_name}'
+    connection.sendmail(sender, user_event.email, message.as_string())
 
 
 def send_by_EmailSender(user_id, event_id):
